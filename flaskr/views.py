c601ef7bfd8e0da809c80003c60ce683625898a2
Sho Tohyama <sho.tohyama@gmail.com>
2017-03-07 23:57:21 +0900

    Add feature of Login/Logout

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,8 +1,36 @@
-from flask import request, redirect, url_for, render_template, flash
+from functools import wraps
+from flask import (abort,
+                   flash,
+                   g,
+                   jsonify,
+                   redirect,
+                   render_template,
+                   request,
+                   session,
+                   url_for)
 from flaskr import app, db
-from flaskr.models import Entry
+from flaskr.models import Entry, User
 
 
+def login_required(f):
+    @wraps(f)
+    def decorated_view(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for('login', next=request.path))
+        return f(*args, **kwargs)
+    return decorated_view
+
+
+@app.before_request
+def load_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(session['user_id'])
+
+
+# Entry
 @app.route('/')
 def show_entries():
     entries = Entry.query.order_by(Entry.id.desc()).all()
@@ -21,27 +49,44 @@ def add_entry():
     return redirect(url_for('show_entries'))
 
 
+# User
 @app.route('/users/')
+@login_required
 def user_list():
-    return 'list users '
+    users = User.query.all()
+    return render_template('user/list.html', users=users)
 
 
 @app.route('/users/<int:user_id>/')
+@login_required
 def user_detail(user_id):
-    return 'user_detail ' + str(user_id)
+    user = User.query.get(user_id)
+    return render_template('user/detail.html', user=user)
 
 
 @app.route('/users/<int:user_id>/edit/', methods=['GET', 'POST'])
+@login_required
 def user_edit(user_id):
-    return 'edit user ' + str(user_id)
+    user = User.query.get(user_id)
+    if user is None:
+        abort(404)
+    if request.method == 'POST':
+        user.name = request.form['name']
+        user.email = request.form['email']
+        user.password = request.form['password']
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('user_detail', user_id=user_id))
+    return render_template('user/edit.html', user=user)
 
 
 @app.route('/users/create/', methods=['GET', 'POST'])
+@login_required
 def user_create():
     if request.method == 'POST':
         user = User(name=request.form['name'],
                     email=request.form['email'],
-                    password=return.form['password'])
+                    password=request.form['password'])
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('user_list'))
@@ -49,5 +94,36 @@ def user_create():
 
 
 @app.route('/users/<int:user_id>/delete/', methods=['DELETE'])
+@login_required
 def user_delete(user_id):
-    return NotImplementedError('DELETE')
+    user = User.query.get(user_id)
+    if user is None:
+        response = jsonify({'status': 'Not Found'})
+        response.status_code = 404
+        return response
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'status', 'OK'})
+
+
+# Logout/login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user, authenticated = User.authenticate(db.session.query,
+                                                request.form['email'],
+                                                request.form['password'])
+        if authenticated:
+            session['user_id'] = user.id
+            flash('ログインしました。')
+            return redirect(url_for('show_entries'))
+        else:
+            flash('メールアドレスかパスワードが間違っています。')
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    flash('ログアウトしました。')
+    return redirect(url_for('show_entries'))
